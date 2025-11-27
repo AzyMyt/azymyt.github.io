ef955713d4e20ef70ed66d40763b9c4828d800a9
AzyMyt <azymyt@proton.me>
2025-11-27 21:13:54 +0000

    Visual overhaul and some rewritten stuff
    HUGE updata

--- things.js
+++ things.js
@@ -2,6 +2,9 @@ const id = "1dIL3JjvLz9QGQPUxDUlIIZmjR2kRAXtgMel7Fyil-j4";
 const range = "Classics!A1:O179";
 const key = "AIzaSyCBmzuL3Z3NORg7j5Jtfq791Y8Hf7Yq0DU";
 
+const thumbHeight = 30 * 9;
+const thumbWidth = 30 * 16;
+
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/${encodeURIComponent(range)}?key=${key}`;
 
 fetch(url)
@@ -22,54 +25,77 @@ fetch(url)
           /(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})/,
         )[1];
         console.log(item.Link, completionID);
-        div.innerHTML = `<a href="${item.Link}" target="_blank">
-        <img src="https://img.youtube.com/vi/${completionID}/maxresdefault.jpg" width=${25 * 16} height=${25 * 9}>
-        </a>
+        div.style.setProperty(
+          "--bg-url",
+          `url("https://img.youtube.com/vi/${completionID}/maxresdefault.jpg")`,
+        );
+        div.innerHTML = `
+          <a href="${item.Link}" target="_blank">
+          <img src="https://img.youtube.com/vi/${completionID}/maxresdefault.jpg" width=${thumbWidth} height=${thumbHeight}>
+          </a>
         `;
-      } else if (item.NLW == "Non-Ex") {
+      } else if (item.NLW == "") {
         return;
-      } else
-        div.innerHTML += `<p width=${25 * 16} height=${25 * 9}>No video</p>`;
+      } else {
+        div.innerHTML += `<img src="novideo.png" width=${thumbWidth} height=${thumbHeight}>`;
+        div.style.setProperty("--bg-url", `url("novideo.png")`);
+      }
 
-      // details
-      details = document.createElement("div");
-      details.classList.add("details");
+      div.innerHTML += `
+      <div class="details">
+        <div class="upperDetails">
+          <h1>#${item.Pos}: ${item.Level} by ${item.Publisher}</h1>
+          <h1 id="date">${item.Date}</h1>
+        </div>
 
-      upperDetails = document.createElement("div");
-      upperDetails.classList.add("upperDetails");
-      upperDetails.innerHTML += `
-        <h1>#${item.Pos}: ${item.Level} by ${item.Publisher}</h1>
-        <h1 id="date">${item.Date}</h1>
-        `;
+        <div class="stats">
+          <div class="table">
+            <div class="block">
+              <p class="aem">${item.AEM}</p>
+              <p class="aemText">AEM</p>
+            </div>
 
-      // stats
-      stats = document.createElement("div");
-      stats.classList.add("stats");
-      stats.innerHTML += `
-      <p class="aem">${item.AEM}</p>
-      <p class="aemText">AEM</p>
-      <p class="nlw">${item.NLW}</p>
-      <p class="nlwText">NLW</p>
+            <div class="block">
+              <p class="gddl">${item.GDDL}</p>
+              <p class="gddlText">GDDL</p>
+            </div>
 
-      <p class="wfText">Worst Fail</p>
-      <p class="attemptsText">Attempts</p>
-      <p class="peakText">Peak</p>
-      <p class="peakListText">Peak List</p>
+            <div class="block">
+              <p class="nlw">${item.NLW}</p>
+              <p class="nlwText">NLW</p>
+            </div>
 
-      <p class="gddl">${item.GDDL}</p>
-      <p class="gddlText">GDDL</p>
-      <p class="enj">${item.Enj}</p>
-      <p class="enjText">ENJ</p>
+            <div class="block">
+              <p class="enj">${item.Enj}</p>
+              <p class="enjText">ENJ</p>
+            </div>
+          </div>
 
-      <p class="wf">${item.WF}</p>
-      <p class="attempts">${item.ATT}</p>
-      <p class="peak">${item.Peak}</p>
-      <p class="peakList">${item.ListPeak}</p>
+          <div class="table">
+            <div class="block">
+              <p class="attempts">${item.ATT}</p>
+              <p class="attemptsText">Attempts</p>
+            </div>
+
+            <div class="block">
+              <p class="wf">${item.WF}</p>
+              <p class="wfText">Worst Fail</p>
+            </div>
+
+            <div class="block">
+              <p class="peak">${item.Peak}</p>
+              <p class="peakText">Peak</p>
+            </div>
+
+            <div class="block">
+              <p class="peakList">${item.ListPeak}</p>
+              <p class="peakListText">Peak List</p>
+            </div>
+          </div>
+        </div>
+      </div>
       `;
 
-      details.appendChild(upperDetails);
-      details.appendChild(stats);
-      div.appendChild(details);
       document.body.appendChild(div);
     });
   });
